f226119ccc245c5f5a9c2103bae12537645c6a0b
Giliard-GC <giliardgodoycastro@gmail.com>
2024-04-23 20:11:18 -0300

    ok

--- ProjetosJava/src/Exercicios22042024/Exercicio02.java
+++ ProjetosJava/src/Exercicios22042024/Exercicio02.java
@@ -1,12 +1,18 @@
 package Exercicios22042024;
 
 public class Exercicio02 {
-
-//Uma pessoa com pouco tempo disponível lê um livro por 5 minutos a cada dia, 
-//6 dias por semana. Monte a fórmula e escreva um programa que calcula e mostra na tela quanto tempo, 
-//em horas, a pessoa terá dedicado ao livro ao final de um ano.
-public static void main(String[] args) {
-    
-    
+    public static void main(String[] args) {
+        int tempoDiario = 5; // em minutos
+        int diasPorSemana = 6;
+        int semanasPorAno = 52;
+        
+        // Calculando o tempo total em minutos
+        int tempoTotalMinutos = tempoDiario * diasPorSemana * semanasPorAno;
+        
+        // Convertendo o tempo total de minutos para horas
+        double tempoTotalHoras = tempoTotalMinutos / 60.0;
+        
+        // Mostrando o resultado na tela
+        System.out.println("Tempo total dedicado ao livro em horas por ano: " + tempoTotalHoras);
     }
 }
