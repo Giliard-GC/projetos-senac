e1fd1218b1f4db06db9af7fe8c23a331385a5f2f
Giliard-GC <giliardgodoycastro@gmail.com>
2024-04-23 20:42:25 -0300

    ok

--- ProjetosJava/src/Exercicios22042024/Exercicio03.java
+++ ProjetosJava/src/Exercicios22042024/Exercicio03.java
@@ -2,6 +2,20 @@ package Exercicios22042024;
 
 public class Exercicio03 {
     public static void main(String[] args) {
-        
+        // Distância em metros de ida e volta para o trabalho
+        int distanciaDiariaMetros = 800;
+
+        // Dias trabalhados por semana e semanas trabalhadas por ano
+        int diasPorSemana = 5;
+        int semanasPorAno = 45;
+
+        // Calcula a distância total percorrida em um ano
+        int distanciaAnualMetros = distanciaDiariaMetros * diasPorSemana * semanasPorAno * 2; // Multiplica por 2 pois vai e volta
+
+        // Converte a distância total de metros para quilômetros
+        double distanciaAnualQuilometros = distanciaAnualMetros / 1000.0;
+
+        // Exibe o resultado
+        System.out.println("Distância total percorrida em um ano: " + distanciaAnualQuilometros + " quilômetros");
     }
 }
--- ProjetosJava/src/Exercicios22042024/Exercicio07.java
+++ ProjetosJava/src/Exercicios22042024/Exercicio07.java
@@ -1,5 +1,20 @@
 package Exercicios22042024;
 
-public class Exercicio07 {
+import java.util.Scanner;
 
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " é um número par.");
+        } else {
+            System.out.println(numero + " é um número ímpar.");
+        }
+
+        scanner.close();
+    }
 }
