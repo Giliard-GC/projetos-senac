9224b39b2977a405b11b8a173a3691d47e8e5f81
Giliard-GC <giliardgodoycastro@gmail.com>
2024-04-23 20:48:35 -0300

    ok

--- ProjetosJava/src/Exercicio26032024/Exercicio114.java
+++ ProjetosJava/src/Exercicio26032024/Exercicio114.java
@@ -1,5 +1,7 @@
 package Exercicio26032024;
 
 public class Exercicio114 {
-
+    public static void main(String[] args) {
+        System.out.println("A vida é o nosso bem mais precioso, é tão forte e capaz de mudar o mundo. Mas ao mesmo tempo é tão frágil, e capaz de terminar em um segundo. Todos os momentos em nossas vidas são mágicos, e cabe a cada um de nós torná-los mais marcantes. Seja feliz e faça alguém feliz também! Estamos todos em busca de amor, amizade, paz, esperança, afeto, sonhos, etc... Não importa, o que vale mesmo é sermos felizes!"); 
+    }
 }
