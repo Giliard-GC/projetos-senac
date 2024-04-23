1c0aca9a77b7cbda84cc538fdc5ec72c8974d7bc
Giliard-GC <giliardgodoycastro@gmail.com>
2024-04-23 20:52:14 -0300

    ok

--- ProjetosJava/src/Exercicio26032024/Exercicio115.java
+++ ProjetosJava/src/Exercicio26032024/Exercicio115.java
@@ -1,5 +1,8 @@
 package Exercicio26032024;
 
 public class Exercicio115 {
-
+    public static void main(String[] args) {
+        System.out.println("Olá professor tudo bem, espero com as aulas aperfeiçoar meus conhecimentos e tentar carreira na área, espero que as aulas sejam de boa explicação e didática para assim ajudar a mim e aos demais colegas da sala.");
+        
+    }
 }
