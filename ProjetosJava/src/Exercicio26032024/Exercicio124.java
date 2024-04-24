5d01adc6ac0329993846e035ef6c38c83a9ce8ca
Giliard-GC <giliardgodoycastro@gmail.com>
2024-04-23 21:33:22 -0300

    ok

--- ProjetosJava/src/Exercicio26032024/Exercicio124.java
+++ ProjetosJava/src/Exercicio26032024/Exercicio124.java
@@ -2,12 +2,12 @@ package Exercicio26032024;
 
 public class Exercicio124 {
     public static void main(String[] args) {
-        int disciplina = 6;
-        int minutos = 40;
-        int hora = 60;
+        double disciplina = 6;
+        double minutos = 40;
+        double hora = 60;
         
 
-        int soma = ((minutos+hora) / disciplina);
+        double soma = ((minutos+hora) / disciplina);
         System.out.println("o resultado é " + soma);
         
     }
--- ProjetosJava/src/Exercicios22042024/Exercicio09.java
+++ ProjetosJava/src/Exercicios22042024/Exercicio09.java
@@ -1,5 +1,14 @@
 package Exercicios22042024;
 
 public class Exercicio09 {
+    public static void main(String[] args) {
+        double disciplina = 6;
+        double minutos = 40;
+        double hora = 60;
+        
 
+        double soma = ((minutos+hora) / disciplina);
+        System.out.println("o resultado é " + soma);
+        
+    }
 }
