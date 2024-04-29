e7b4de19107daaf03abff89e575443393bc3d09c
Giliard <giliardgodoycastro@gmail.com>
2024-04-29 20:36:56 -0300

    ok

---
+++ ProjetosJava/src/Exercicios29042024/Exercicio01.java
@@ -0,0 +1,15 @@
+package Exercicios29042024;
+
+public class Exercicio01 {
+
+    public static void main(String[] args){
+        int contador = 0;
+        int numero = 15;
+        
+        while (contador <= numero){
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    
+    }
+}
---
+++ ProjetosJava/src/Exercicios29042024/Exercicio02.java
@@ -0,0 +1,15 @@
+package Exercicios29042024;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int redutor = 10;
+        int contador = 0;
+        int total = redutor;
+
+        while (contador <= total){
+            System.out.println("Contador: " + contador);
+            redutor = redutor - 1;
+            contador++;
+        }
+    }
+}
