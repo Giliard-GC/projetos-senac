f0f03845febd1906704238070d0d6c39b489f790
Giliard-GC <giliardgodoycastro@gmail.com>
2024-05-06 21:38:35 -0300

    ok

--- ProjetosJava/src/Exercicios29042024/Exercicio04.java
+++ ProjetosJava/src/Exercicios29042024/Exercicio04.java
@@ -1,27 +1,44 @@
 package Exercicios29042024;
 
- 
-import java.util.Scanner;
- 
+
 public class Exercicio04 {
-  public static void main(String[] args) {
-  
-    Scanner entrada = new Scanner(System.in);
-     
-    // vamos pedir o número inteiro
-    System.out.print("Informe o primeiro número (dividendo): ");
-    int dividendo = Integer.parseInt(entrada.nextLine());
-     
-    int quociente = 0; // vai guardar o resultado da divisão
-     
-    // enquanto o dividendo for maior ou igual ao
-    // divisor
-    while(dividendo >= divisor){
-      dividendo = dividendo - divisor; // subtrai o divisor do dividendo
-      quociente++; // aumenta o quociente em 1
+    // Crie um algoritmo que recebe 1 número e verifica se o mesmo é perfeito.
+    // Um numero é perfeito quando a soma dos seus divisores é igual a ele mesmo, por exemplo 6.
+    // Para isso use resto e acumulador soma.
+    // Verificando se numero e perfeito:
+    // Exemplo para numero 6
+    // POSICAO 01 - resto = 6 % 5;
+    // POSICAO 02 - resto = 6 % 4;
+    // POSICAO 03 - resto = 6 % 3;
+    // POSICAO 04 - resto = 6 % 2;
+    // POSICAO 05 - resto = 6 % 1;
+    // Para cada posicao, verificar se o resto = 0 , adiciona a soma numa variavel de nome soma.
+    // Apos passar por todas as repetições, no final se a soma == numero, o numero e perfeito.
+
+    public static void main(String[] args) {
+        
+        int numero = 6;
+        int numero_inicial = numero;
+
+        int contador = 1;
+        int total_linhas = numero;
+
+        int resto = 0;
+        int soma = 0;
+
+        while (contador < total_linhas) {
+            numero--;
+            resto = numero_inicial % numero;
+
+            if (resto == 0) {
+                soma = soma + numero;
+            }
+            
+            contador++;
+        }
+        
+        if (soma == numero_inicial) {
+            System.out.println("O numero " + numero_inicial + " é perfeito");
+        }
     }
-     
-    // agora mostramos o resultado da divisão
-    System.out.println("O resultado da divisão é: " + quociente); 
-  }
 }
---
+++ ProjetosJava/src/Exercicios30042024/Exercicio04.java
@@ -0,0 +1,7 @@
+package Exercicios30042024;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        
+    }
+}
